dbbf8bee24df267091415235dca4a0cee6b3a8da
Nishant Deb <nishant.deb@jungelworks.co>
2018-10-31 16:51:39 +0530

    tracking

---
+++ tracking/db.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/trackig_db');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log(' connected');
+});
+
+module.exports ={
+  db
+}
